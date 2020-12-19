c7928a5970f239e62d3a3a77722b4549e008abf6
KornSiwat <siwatponpued@gmail.com>
2020-12-19 19:39:59 +0700

    refactored

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,84 @@
 import express from "express"
 import "reflect-metadata"
-import { createConnection } from "typeorm"
+import {
+  Connection,
+  createConnection,
+  getConnectionOptions,
+  ConnectionOptions,
+} from "typeorm"
 import { SETInsiderScraperApplication } from "./SETInsiderScraperApplication"
 import { createRouter } from "./router"
-import { stockSymbols } from "./stockSymbols"
+import { SET50Symbols } from "./stockSymbols"
 import cors from "cors"
-import { getConnectionOptions, ConnectionOptions } from "typeorm"
 import dotenv from "dotenv"
 
 dotenv.config()
 
-async function getOptions(): Promise<ConnectionOptions> {
+async function getDatabaseConnectionOptions(): Promise<ConnectionOptions> {
   let connectionOptions: ConnectionOptions
   connectionOptions = {
     type: "postgres",
     synchronize: true,
     logging: false,
-    entities: ["dist/entity/*.*"],
+    entities: ["dist/models/*.js"],
+    migrations: ["dist/migration/**/*.js"],
+    subscribers: ["dist/subscriber/**/*.js"],
   }
 
-  if (process.env.DATABASE_URL) {
+  const isDatabaseURLExistInENV = process.env.DATABASE_URL !== undefined
+
+  if (isDatabaseURLExistInENV) {
     Object.assign(connectionOptions, { url: process.env.DATABASE_URL })
   } else {
-    // gets your default configuration
-    // you could get a specific config by name getConnectionOptions('production')
-    // or getConnectionOptions(process.env.NODE_ENV)
     connectionOptions = await getConnectionOptions()
   }
 
   return connectionOptions
 }
 
+async function connectToDatabase(
+  databaseConnectionOptions: ConnectionOptions
+): Promise<Connection> {
+  return createConnection(databaseConnectionOptions)
+}
+
+async function setupDatabase(connection: Connection) {
+  await connection.synchronize()
+}
+
+async function startServer() {
+  const app = express()
+  const port = process.env.PORT || 3000
+  const onGoingScrapeLimitCount = 5
+
+  // SET50Symbols is used because free tier database hava small limit size so set50 is enough for demonstration
+  const setInsiderScraperApplication = new SETInsiderScraperApplication(
+    SET50Symbols,
+    onGoingScrapeLimitCount
+  )
+  const router = createRouter(setInsiderScraperApplication)
+
+  app.use(cors())
+  app.use(express.json())
+  app.use(router)
+
+  app.listen(port, () =>
+    console.log(`SET Insider Scraper Application is on port ${port}!`)
+  )
+}
+
 async function main() {
-  createConnection(await getOptions())
-    .then((_) => {
-      const app = express()
-      const port = process.env.PORT || 3000
-      const onGoingScrapeLimitCount = 5
+  const databaseConnectionOptions = await getDatabaseConnectionOptions()
 
-      const setInsiderScraperApplication = new SETInsiderScraperApplication(
-        stockSymbols,
-        onGoingScrapeLimitCount
-      )
-      const router = createRouter(setInsiderScraperApplication)
+  try {
+    const connection = await connectToDatabase(databaseConnectionOptions)
 
-      app.use(cors())
-      app.use(express.json())
-      app.use(router)
+    await setupDatabase(connection)
+  } catch (error) {
+    console.log(`fail to connect db due to + ${error}`)
+  }
 
-      app.listen(port, () =>
-        console.log(`SET Insider Scraper Application is on port ${port}!`)
-      )
-    })
-    .catch((error) => console.log(`fail to connect db due to + ${error}`))
+  await startServer()
 }
 
 main()
